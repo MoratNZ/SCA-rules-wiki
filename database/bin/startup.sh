84c46842d963b18ea480543e2ba3a97c1931554f
David Maclagan <david@verso.org>
2024-06-14 11:29:15 +1200

    Updates startup scripts for database
    Copies in sql backup rather than direct mounting it.
    
    Updates mediawiki user password to match the env variable

--- database/bin/startup.sh
+++ database/bin/startup.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
+
+echo "Setting up cron"
 sed -i "s/mysqlpassword/$MYSQL_ROOT_PASSWORD/" /etc/crontab
 /etc/init.d/cron start
+
+echo removing default bootstrap.sh
+rm /docker-entrypoint-initdb.d/bootstrap.sh
+
+echo Moving latest backup to bootstrap folder
+cp /db_backups/latest.sql /docker-entrypoint-initdb.d/latest.sql
+chmod 666 /docker-entrypoint-initdb.d/latest.sql
+
+echo Updating password and perms for mediawiki user \'$MYSQL_USER\'
+echo "GRANT ALL ON $DB_NAME.* TO '$MYSQL_USER'@'%';" >> /docker-entrypoint-initdb.d/latest.sql
+echo "ALTER USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> /docker-entrypoint-initdb.d/latest.sql
 
 exec /entrypoint.sh mysqld

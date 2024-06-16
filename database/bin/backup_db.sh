fbecd11d836e2c24eecfd4f9490af8af04bb1e53
David Maclagan <david@verso.org>
2024-06-16 12:43:50 +1200

    db_back - Creates archive dir if missing

--- database/bin/backup_db.sh
+++ database/bin/backup_db.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# create the archive dir if it doesn't exist.
+if [ ! -d $BACKUPS_LOCATION/archive ];
+    then;
+    mkdir -p $BACKUPS_LOCATION/archive;
+fi
+
 # Dump the database to /db_backups
 FILENAME=$(date +"%Y%m%d%H%M")_mediawiki
 /usr/bin/mysqldump --add-drop-database -u root -p$MYSQL_ROOT_PASSWORD --all-databases > $BACKUPS_LOCATION/archive/$FILENAME.sql
@@ -7,4 +13,4 @@ FILENAME=$(date +"%Y%m%d%H%M")_mediawiki
 # remove problematic attempt to drop 'mysql' database
 /usr/bin/sed -i 's/\/\*!40000 DROP DATABASE IF EXISTS `mysql`\*\/;/ /g' $BACKUPS_LOCATION/archive/$FILENAME.sql
 
-/usr/bin/cp /docker-entrypoint-initdb.d/archive/$FILENAME.sql $BACKUPS_LOCATION/latest.sql
+/usr/bin/cp $BACKUPS_LOCATION/archive/$FILENAME.sql $BACKUPS_LOCATION/latest.sql

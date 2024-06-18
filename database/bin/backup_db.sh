fb85553f63a573f6ff17adda390a74f54916a7f8
David Maclagan <david@verso.org>
2024-06-19 09:37:24 +1200

    Changes db backup script to only saving dump if the db has changed

--- database/bin/backup_db.sh
+++ database/bin/backup_db.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
 # create the archive dir if it doesn't exist.
-if [ ! -d $BACKUPS_LOCATION/archive ];
-    then;
-    mkdir -p $BACKUPS_LOCATION/archive;
+if [ ! -d $BACKUPS_LOCATION/archive ]; then
+    /usr/bin/mkdir -p $BACKUPS_LOCATION/archive
 fi
 
 # Dump the database to /db_backups
-FILENAME=$(date +"%Y%m%d%H%M")_mediawiki
-/usr/bin/mysqldump --add-drop-database -u root -p$MYSQL_ROOT_PASSWORD --all-databases > $BACKUPS_LOCATION/archive/$FILENAME.sql
+/usr/bin/mysqldump --add-drop-database -u root -p$MYSQL_ROOT_PASSWORD --all-databases > $BACKUPS_LOCATION/candidate.sql
 
 # remove problematic attempt to drop 'mysql' database
-/usr/bin/sed -i 's/\/\*!40000 DROP DATABASE IF EXISTS `mysql`\*\/;/ /g' $BACKUPS_LOCATION/archive/$FILENAME.sql
+/usr/bin/sed -i 's/\/\*!40000 DROP DATABASE IF EXISTS `mysql`\*\/;/ /g' $BACKUPS_LOCATION/candidate.sql
 
-/usr/bin/cp $BACKUPS_LOCATION/archive/$FILENAME.sql $BACKUPS_LOCATION/latest.sql
+# Compare the candidate dump with the latest dump
+if /usr/bin/diff --brief <(head -n -1 $BACKUPS_LOCATION/latest.sql) <(head -n -1 $BACKUPS_LOCATION/candidate.sql) > /dev/null ; then 
+    /usr/bin/echo "No change to the database"
+else
+    /usr/bin/echo "Database changed; saving backup"
+    ARCHIVE_FILENAME=$(date +"%Y%m%d%H%M")_mediawiki.sql
+    /usr/bin/cp $BACKUPS_LOCATION/candidate.sql $BACKUPS_LOCATION/archive/$ARCHIVE_FILENAME.sql
+    /usr/bin/cp $BACKUPS_LOCATION/candidate.sql $BACKUPS_LOCATION/latest.sql
+fi
+
+# Clean up candidate file
+rm $BACKUPS_LOCATION/archive/candidate.sql

b9b4432af42e712febd19d5a79c7d7531da83600
David Maclagan <david@verso.org>
2024-06-19 12:37:31 +1200

    Adds usage info to image backup

--- mediawiki/bin/backup_images.sh
+++ mediawiki/bin/backup_images.sh
@@ -1,14 +1,36 @@
 #!/bin/bash
+PROGRAM_NAME=$0
+
+function usage {
+    echo "usage: $PROGRAM_NAME [-fh]"
+    printf "
+This script backs up the mediawiki images directory.
+Unless the -f flag is set, the backup will only be saved if something has in
+the images tree has changed since the last backup was taken.
+
+Backup files are stored at the location set in the \$BACKUPS_LOCATION 
+environment variable (in the current environment this is set to 
+\`$BACKUPS_LOCATION\`) as latest.tar.gz, and in \$BACKUPS_LOCATION/archive/ as 
+[ datestamp ]_images.tar.gz (e.g., 202406182110_images.tar.gz
+
+"
+    echo "  -f      Force a backup, even if there have been no changes to the database"
+    echo "  -h      display help"
+    exit 1
+}
+
 FORCE=false
 # Process command line flags
-while getopts 'f' OPTION; do
+while getopts 'fh' OPTION; do
     case "$OPTION" in
         f) 
             FORCE=true
             ;;
+        h)
+            usage
+            ;;
     esac
 done
-
 
 # Check the age of the backup
 BACKUP_AGE=`/usr/bin/find $BACKUPS_LOCATION/latest.tar.gz -type f -printf '%T@'`
